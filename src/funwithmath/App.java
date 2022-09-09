ac90635347d65370c2bffd21e5e44735bcc8d103
klumpra <klumpra@lewisu.edu>
2022-09-09 12:30:33 -0500

    added funwithmath

---
+++ src/funwithmath/App.java
@@ -0,0 +1,11 @@
+package funwithmath;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println(Math.PI);
+		System.out.println(Math.E);
+		double area;
+		area = Math.PI * Math.pow(5,2);
+		System.out.println(area);
+	}
+}
