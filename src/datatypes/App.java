1df05d740e21643099586e853092ad56d13716f4
klumpra <klumpra@lewisu.edu>
2022-09-09 12:27:13 -0500

    First commit

---
+++ src/datatypes/App.java
@@ -0,0 +1,13 @@
+package datatypes;
+
+public class App {
+	public static void main(String[] args) {
+		int luckyNumber = 7;
+		int unluckyNumber = 13;
+		double value = 13.5432;
+		double anotherValue = 7.47;
+		double quotient = value/anotherValue;
+		double result = (double)luckyNumber/ unluckyNumber;
+		System.out.println(result);
+	}
+}
